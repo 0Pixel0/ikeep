4929e3790c0c29f1d195133edb0a486e66d801b8
Ashutosh <ashutosh.punia@yandex.com>
2021-12-23 02:37:45 +0530

    modified src files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateNote from "./components/CreateNote";
 
 function App() {
+
+  const [noteContent, setNoteContent] = useState([]);
+
+  const [noteTitle, setNoteTitle] = useState("");
+
+  const [noteDescription, setNoteDescription] = useState("");
+
+  let titleChangeHandler = (event) => setNoteTitle(event.target.value);
+
+  let descriptionChangeHandler = (event) =>setNoteDescription(event.target.value);
+
+  let addHandler = (event) => {
+    setNoteContent(previous => {
+      return [...previous, { title: noteTitle, content: noteDescription }];
+    });
+    setNoteTitle("");
+    setNoteDescription("");
+    event.preventDefault();
+  };
+
+  let deleteNote = (id) => {
+    setNoteContent((previous) => {
+      return previous.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header></Header>
+      <CreateNote
+        title={noteTitle}
+        content={noteDescription}
+        changeInputHandler={titleChangeHandler}
+        changeTextHandler={descriptionChangeHandler}
+        addNoteHandler={addHandler}
+      />
+      {noteContent.map((note, index) => {
+        return (
+          <Note
+            id={index}
+            key={index}
+            title={note.title}
+            content={note.content}
+            delNote={deleteNote}
+          ></Note>
+        );
+      })}
+      <Footer></Footer>
     </div>
   );
 }
